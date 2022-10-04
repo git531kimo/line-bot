bd6139208ec8a752e9be93e9bf1aa6142cc8b3a7
Frank Shiau <git531kimo@yahoo.com.tw>
2022-10-04 18:50:29 +0800

    update

--- app.py
+++ app.py
@@ -40,10 +40,14 @@ def handle_message(event):
     msg = event.message.text
     r = '我看不懂'
 
-    if mag == 'hi':
+    if mag in ['hi', 'Hi', 'HI']:
         r = 'hi'
     elif mag == '吃飯了嗎':
         r = '還沒'
+    elif msg == '你是誰':
+        r = '我是機器人'
+    elif '訂位' in msg:
+        r = '你想訂什麼時候'
 
     line_bot_api.reply_message(
         event.reply_token,
